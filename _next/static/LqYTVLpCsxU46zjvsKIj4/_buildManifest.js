b3800ec0e0c7ac3682cabb5109a046e1d30e4b2d
Debashish Roy <debaroy70@gmail.com>
2025-12-23 22:22:53 +0600

    Updates

---
+++ _next/static/LqYTVLpCsxU46zjvsKIj4/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/debashish-portfolio//_next/:path+",
+        "destination": "/debashish-portfolio/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
